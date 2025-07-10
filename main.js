d554c3b88784c448b4f6d013e20a87031230285b
Severinkirill <severinkirill09@gmail.com>
2025-07-10 11:32:55 +0300

    h/w

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createSummator() {
 
 const sum = createSummator();
 
-console.log(sum(4));  // 4
-console.log(sum(6));  // 10
-console.log(sum(10)); // 20
-console.log(sum(7));  // 27
+console.log(sum(4));
+console.log(sum(6));
+console.log(sum(10));
+console.log(sum(7));
